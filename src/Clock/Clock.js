f8bbad28825ddff35f39cf14516ad3994b9b28c8
Valois, Marcos (wintersocram) <valois.marcos@gmail.com>
2019-12-15 17:05:56 -0500

    clock: passing setInterval to component using lifecycle methods

--- src/Clock/Clock.js
+++ src/Clock/Clock.js
@@ -2,11 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class Clock extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { date: new Date() };
+    }
+
+    tick() {
+        this.setState({ date: new Date() });
+    }
+
+    // lifecycle methods
+    componentDidMount() {
+        this.timer = setInterval(
+            () => this.tick(),
+            1000
+        )
+    }
+    componentWillUnmount() {
+        clearInterval(this.timer);
+    }
+
+    // render method
     render() {
         return (
             <div>
                 <Welcome name={ data.name } />
-                <CurrentTime date={ data.date(this.props.date) } />
+                <CurrentTime date={ data.date(this.state.date) } />
             </div>
         );
     }
@@ -24,10 +45,10 @@ function CurrentTime(props) {
     return <h2>It is { props.date }</h2>;
 }
 
-function tick() {
-    ReactDOM.render(<Clock date={ new Date() } />, document.getElementById('root'));    
-}
 
 export default function SetClock() {
-    setInterval(tick, 1000);
+    ReactDOM.render(
+        <Clock />, 
+        document.getElementById('root')
+    );
 }
