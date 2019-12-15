a8ba6b06775b25250e4d080d8c489fa5801912af
Valois, Marcos (wintersocram) <valois.marcos@gmail.com>
2019-12-15 18:00:55 -0500

    clock: btn to stop clock

--- src/Clock/Clock.js
+++ src/Clock/Clock.js
@@ -5,25 +5,43 @@ import CurrentTime from './CurrentTime'
 export default class Clock extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { date: new Date() };
+        this.state = { 
+            date: new Date(), 
+            updateTick: true
+        };
         this.data = {
             name: "John Doe",
             date: date => date.toLocaleTimeString()
         };
-    }
+        this.handleTick = this.handleTick.bind(this);    }
 
+    // Check after: handleTick is not re-rendering
+    handleTick(e) {
+        e.preventDefault();
+        // incorrect: state is assynchronous
+        // this.setState({ updateTick: !this.state.updateTick });
+        // correct
+        this.setState((state) => ({
+            updateTick: !state.updateTick
+        }));
+    }
     tick() {
         this.setState({ date: new Date() });
     }
 
     // lifecycle methods
+    componentWillMount() {
+        console.log('[componentWillMount()] - state: %o', this.state);
+    }
     componentDidMount() {
+        console.debug('[componentDidMount()] - state: %o', this.state);
         this.timer = setInterval(
-            () => this.tick(),
+            () => this.state.updateTick ? this.tick() : null,
             1000
         )
     }
     componentWillUnmount() {
+        console.log('[componentWillUnmount()] - state: %o', this.state);
         clearInterval(this.timer);
     }
 
@@ -33,7 +51,17 @@ export default class Clock extends React.Component {
             <div>
                 <Welcome name={ this.data.name } />
                 <CurrentTime date={ this.data.date(this.state.date) } />
+                <BtnHandleTick handleClick={ this.handleTick } tickStatus={ this.state.updateTick } />
             </div>
         );
     }
+}
+
+function BtnHandleTick(props) {
+    console.log('[BtnHandleTick]. ');
+    return (
+        <a href="#" onClick={props.handleClick}>
+            { props.tickStatus ? 'stop' : 'start'} the clock
+        </a>
+    )
 }
