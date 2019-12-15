44ae7204084e27056498b8d4e862813ac4abea07
Valois, Marcos (wintersocram) <valois.marcos@gmail.com>
2019-12-15 15:56:13 -0500

    hello world: conditional

--- src/HelloWorld/HelloWorld.js
+++ src/HelloWorld/HelloWorld.js
@@ -11,49 +11,15 @@ const user = {
     firstName: 'John',
     lastName: 'Doe'
 };
-const place = 'World';
+
+function getGreeting(user) {
+    if (user)
+        return <h1>Hello, {formatName(user)}!</h1>;
+    return <h1>Hello, Stranger.</h1>;
+}
 
 function HelloWorld() {
-    const elementName = (
-        <h1>
-            Hello, { formatName(user) }!!!
-        </h1>
-    );
-    // const elementPlace = <h2>Hello, { place }!!!</h2>;
-
-    // It will generate an error:
-    // Adjacent JSX elements must be wrapped in an enclosing tag. Did you want a JSX fragment <>...</>?
-    // const elementName = (
-    //     <h1>
-    //         Hello, { name }!!!
-    //     </h1>
-    //     <h1>
-    //         Hello, { name }!!!
-    //     </h1>
-    // );
-    // However, this will be ok:
-    // const elementName = (
-    //     <div>
-    //         <h1>
-    //             Hello, { name }!!!
-    //         </h1>
-    //         <h1>
-    //             Hello, { name }!!!
-    //         </h1>
-    //     </div>
-    // );
-
-    // Returns the last
-    // return elementName && elementPlace;
-
-    // Returns [object Object][object Object]
-    // return elementName + elementPlace;
-
-    // Returns object
-    console.log('%o, %o', typeof elementName, elementName);
-    // return typeof elementName;
-
-    // Returns the object React
-    return elementName;
+    // return getGreeting(user);
+    return getGreeting();
 }
 export default HelloWorld;
